403bb8b0b7debae0f1a2ea3e92aff0f4a007a73e
11mahek <159419933+11mahek@users.noreply.github.com>
2025-06-23 23:53:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if st.session_state.step == 0:
 # Step 1: Generate quiz
 elif st.session_state.step == 1:
     prompt = (
-        f"Generate enough multiple-choice questions for a person who completely confused in his life."
+        f"Generate  multiple-choice questions for a person who completely confused in his life."
         f"for a {st.session_state.age}-year-old interested in {st.session_state.interest}. "
         "we can easily suggest him best future career. "
         f"Each question should be standalone and include 4 answer options labeled A, B, C, and D. "
